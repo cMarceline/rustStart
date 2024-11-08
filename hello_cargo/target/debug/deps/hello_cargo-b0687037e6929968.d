0c33a09b7ee2868efe17ad31a44be03e82d5d2ea
System administrator <mitchcollins12@gmail.com>
2024-11-09 00:44:36 +1100

    Migrated to Cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-b0687037e6929968.d
@@ -0,0 +1,5 @@
+/home/marci/rustStart/hello_cargo/target/debug/deps/hello_cargo-b0687037e6929968: src/main.rs
+
+/home/marci/rustStart/hello_cargo/target/debug/deps/hello_cargo-b0687037e6929968.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/marci/rustStart/hello_cargo/target/debug/hello_cargo: /home/marci/rustStart/hello_cargo/src/main.rs
